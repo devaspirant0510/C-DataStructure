68102eb2bccfe08161837c6e1007c1c7975cacf2
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-11-02 19:49:59 +0900

    원형큐 템플릿 클래스 구현

--- DataStructure/Queue/main.cpp
+++ DataStructure/Queue/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include "myQueue.hpp"
 
 using namespace std;
-
+typedef struct {
+	char name[20];
+	int age;
+} Person;
+Person createPerson(char* name, int age);
 int main() {
+	Queue<int> q = Queue<int>(9);
+	q.enqueue(1);
+	q.enqueue(2);
+	q.enqueue(3);
+	q.enqueue(4);
+	q.printQueue();
+	cout << "dequeue : " <<q.dequeue() << endl;
+	cout << "dequeue : " <<q.dequeue() << endl;
+	q.printQueue();
+	cout << "queue head : " << *(q.head()) << " queue tail : " << *(q.tail()) << endl;
+	Queue<Person> structQ = Queue<Person>(20);
+	char name1[20] = "seungho";
+	structQ.enqueue(createPerson(name1, 20));
+	cout << structQ.head()->age<< endl;
+	cout << structQ.head()->name << endl;
 
 	return 0;
+}
+Person createPerson(char* name, int age) {
+	Person person;
+	strcpy_s(person.name, 20,name);
+	person.age = age;
+	return person;
 }
--- DataStructure/Queue/myQueue.hpp
+++ DataStructure/Queue/myQueue.hpp
@@ -1,1 +1,135 @@
 #pragma once
+#include <iostream>
+
+#define MAX_QUEUE_SIZE 10000
+
+using namespace std;
+
+template<typename T>
+class Queue
+{
+	private:
+		int maxSize;
+		char QUEUE_IS_EMPTY[20] = "Queue is empty";
+		char QUEUE_IS_FULL[20] = "Queue is full";
+
+	public:
+		int front=0;
+		int rear=0;
+		int tail_val;
+		T *queue;
+		Queue();
+		Queue(int max_size);
+		void sendErrorMsg(char* msg);
+		bool isEmpty();
+		bool isFull();
+		int getSize();
+		void enqueue(T element);
+		T dequeue();
+		T* head();
+		T* tail();
+		void printQueue();
+};
+
+template <typename T>
+Queue<typename T>::Queue() {
+	this->queue = new T[MAX_QUEUE_SIZE];
+	this->maxSize = MAX_QUEUE_SIZE;
+	front = rear = 0;
+}
+
+template <typename T>
+Queue<typename T>::Queue(int max_size) {
+	this->queue = new  T[max_size];
+	this->maxSize = max_size;
+	front = rear = 0;
+}
+
+template<typename T>
+void Queue<typename T>::sendErrorMsg(char* msg) {
+	std::cout << msg << std::endl;
+}
+
+template<typename T>
+bool Queue<typename T>::isEmpty() {
+	if (front == rear) {
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+bool Queue<typename T>::isFull() {
+	if ((rear+1)%this->maxSize==front) {
+		return true;
+	}
+	return false;
+
+
+}
+
+template<typename T>
+int Queue<typename T>::getSize() {
+	if (front < rear) {
+		return rear - front;
+	}
+	else {
+		return rear + maxSize - front;
+	}
+}
+
+template<typename T>
+void Queue<typename T>::enqueue(T element) {
+	if (isFull()) {
+		sendErrorMsg(QUEUE_IS_FULL);
+		return;
+	}
+	queue[rear] = element;
+	tail_val = rear;
+	rear = (rear + 1) % maxSize;
+}
+template<typename T>
+T Queue<typename T>::dequeue() {
+	if (isEmpty()) {
+		sendErrorMsg(QUEUE_IS_EMPTY);
+		return NULL;
+	}
+	int deqPos = front;
+	front = (front + 1) % maxSize;
+	
+	return queue[deqPos];
+}
+
+//template<typename T>
+//T Queue<typename T>::front() {
+//	if (isEmpty()) {
+//		sendErrorMsg("stack is empty");
+//		return;
+//	}
+//	return queue[front];
+//}
+template<typename T>
+T* Queue<typename T>::head() {
+	if (isEmpty()) {
+		sendErrorMsg(QUEUE_IS_EMPTY);
+		return NULL;
+	}
+	return &queue[front]; 
+}
+
+template<typename T>
+T* Queue<typename T>::tail() {
+	if (isEmpty()) {
+		sendErrorMsg(QUEUE_IS_EMPTY);
+		return NULL;
+	}
+	return &queue[tail_val]; 
+}
+
+template<typename T>
+void Queue<typename T>::printQueue() {
+	for (int i = front; i < front + this->getSize(); i++) {
+		cout << queue[i % maxSize] << " , ";
+	}
+	cout << endl;
+}
