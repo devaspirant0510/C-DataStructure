543f3aab403332fc38572a70e9ae70e81e41529b
devaspirant0510 <seungho020510@gmail.com>
2022-10-05 23:12:16 +0900

    feat : is_complete_binary_tree 구현

--- DataStructure/Tree/BTree2.hpp
+++ DataStructure/Tree/BTree2.hpp
@@ -1,13 +1,54 @@
+#pragma once
+
 #include "BTree.hpp"
 
 template<typename T>
 class BTree2 : public BTree<T>{
     public:
-        bool is_complete_binary_tree(); //TODO : 완전이진트리인지 여부
+        bool is_complete_binary_tree(TreeNode<T>*); //TODO : 완전이진트리인지 여부
         int get_node_level(TreeNode<T>*); //TODO : 노드의 레벨
         bool is_balanced(); // TODO : 밸런스한지 (양쪽 서브트리의 높이 차가 1 이하일경우)
         int path_length(); // TODO : 루트부터 모든 자식까지의 경로의 길이 합 
         void reverse(); // TODO : 트리를 좌우 대칭시킴
-
+    private:
+        int find_level(TreeNode<T>*,TreeNode<T>*);
 
 };
+
+template<typename T>
+bool BTree2<T>::is_complete_binary_tree(TreeNode<T>* node){
+    if(node->left_tree !=NULL && node->right_tree != NULL){
+        return is_complete_binary_tree(node->left_tree) && is_complete_binary_tree(node->right_tree);
+    }
+    else if (node->left_tree==NULL && node ->right_tree==NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+template<typename T>
+int BTree2<T>::get_node_level(TreeNode<T>*find_node){
+    return find_level(this->root,find_node);
+}
+
+template<typename T>
+int BTree2<T>::find_level(TreeNode<T> *root,TreeNode<T> *find_node){
+    if(root==NULL){
+        return 0;
+    }
+    if (root==find_node){
+        return 1;
+    }
+
+    else{
+        int left = find_level(find_node->left_tree,find_node);
+        int right = find_level(find_node->right_tree,find_node);
+       // if(left==0 && right==0){
+         //   return 0;
+        //}
+        return left>right ?left:right;
+
+    }
+}
--- DataStructure/Tree/BinaryTree.cpp
+++ DataStructure/Tree/BinaryTree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "BTree.hpp"
+#include "BTree2.hpp"
 
 using namespace std;
 
@@ -22,20 +23,22 @@ int FolderBTree::get_folder_size(TreeNode<FolderTreeType> *root)
 
 int main()
 {
-    FolderBTree btree;
-    int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 67;
+    BTree2<int> btree;
+    int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 67, h=11;
     TreeNode<int> *f_tree = btree.create_tree(&f, NULL, NULL);
     TreeNode<int> *e_tree = btree.create_tree(&e, NULL, NULL);
     TreeNode<int> *g_tree = btree.create_tree(&g, NULL, NULL);
-    TreeNode<int> *d_tree = btree.create_tree(&d, NULL, g_tree);
+    TreeNode<int> *d_tree = btree.create_tree(&d, NULL, NULL);
     TreeNode<int> *b_tree = btree.create_tree(&b, d_tree, e_tree);
-    TreeNode<int> *c_tree = btree.create_tree(&c, f_tree, NULL);
+    TreeNode<int> *c_tree = btree.create_tree(&c, f_tree, g_tree);
     TreeNode<int> *a_tree = btree.set_root(&a, b_tree, c_tree);
 
     btree.preorder(btree.root);
     cout << "노드의 개수 : " << btree.count_node(btree.root) << endl;
     cout << "노드의 높이 : " << btree.get_height(btree.root) << endl;
-    cout << "폴더 크기 : " << btree.get_folder_size(btree.root) << endl;
+    cout << "이진트리인가요? : " << btree.is_complete_binary_tree(btree.root) << endl;
+    cout << "f_tree는 몇층에 있게요? : " << btree.get_node_level(f_tree) << endl;
+    //cout << "폴더 크기 : " << btree.get_folder_size(btree.root) << endl;
 
     return 0;
 }
