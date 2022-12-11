cf970b9c4674b1ef0b4ea346798653afbe7cc672
devaspirant0510 <seungho020510@gmail.com>
2022-12-11 19:02:52 +0900

    feat : bstree remove,add operation test

--- DataStructure/Tree/BinarySearchTree.cpp
+++ DataStructure/Tree/BinarySearchTree.cpp
@@ -4,28 +4,36 @@
 
 using namespace std;
 
+struct s{
+    int a;
+    s* refer;
+};
+int ** arr2r(){
+    int **a = new int*[10];
+    int a1 = 3;
+    a[0] = &a1;
+    return a;
+}
 int main(){
     BSTree<int> bstree;
-    int a_v = 4;
+    int a_v = 10;
     int b_v = 2;
     int c_v = 8;
     TreeNode<int>* aaa = bstree.ct(1,NULL,NULL);
     TreeNode<int>* b = bstree.create_tree(&b_v,NULL,NULL);
     TreeNode<int>* c = bstree.create_tree(&c_v,NULL,NULL);
     bstree.set_root(&a_v,b,c);
-    //bstree.print_tree(bstree.root);
-    int insert_ = 10;
+    bstree.print2_tree();
+    int insert_ = 1;
     TreeNode<int>* insert_val = bstree.create_tree(&insert_,NULL,NULL);
     bstree.insert(bstree.root,insert_val);
-    cout << "1:  " << *(bstree.root->value) << endl ;
-    cout << "2:  " << *(bstree.root->left_tree->value) << endl ;
-    cout << "3:  " << *(bstree.root->left_tree->left_tree->value) << endl ;
-    //cout << "2:  " << *(bstree.root->left_tree->left_tree->value) << endl ;
-    bstree.print_tree(bstree.root);
-
-    cout << "result : " <<bstree.search(bstree.root,1)<<endl;
-    
-
-
+    bstree.print2_tree();
+    bstree.test_insert(bstree.root,4);
+    bstree.print2_tree();
+    bstree.test_insert(bstree.root,6);
+    bstree.print2_tree();
+    bstree.test_insert(bstree.root,11);
+    bstree.print2_tree();
+    //bstree.remove(bstree.root,8);
     return 0;
 }
