a7e14e65b3e9f557001d1f159983971d11677bb3
devaspirant0510 <seungho020510@gmail.com>
2022-12-11 19:01:56 +0900

    feat : tree print use queue

--- DataStructure/Tree/BSTree.hpp
+++ DataStructure/Tree/BSTree.hpp
@@ -9,10 +9,55 @@ class BSTree:public BTree<T>{
     public:
         int search(TreeNode<T>*,T);
         int insert(TreeNode<T>*,TreeNode<T>*);
+        int test_insert(TreeNode<T>*,T);
+        int remove(TreeNode<T>*, T);
 
     private:
+        int insert_refer(TreeNode<T>*,T&);
+        TreeNode<T>** search_return(TreeNode<T>*,TreeNode<T>*,T);
 };
 
+template<typename T>
+int BSTree<T>::remove(TreeNode<T>* root,T value){
+    TreeNode<T>** result = search_return(root,root,value);
+    TreeNode<T>* ro = *result;
+    TreeNode<T>* pa = *(result +1);
+    TreeNode<T>* del_tree;
+    cout << "root : " <<*(ro->value) << endl;
+    cout << "parent : " <<*(pa->value) << endl;
+    if(ro->left_tree==NULL && ro->right_tree==NULL){
+        if(*(pa->value)>*(ro->value)){
+            del_tree = pa->left_tree;
+            //delete del_tree;
+            pa->left_tree = NULL;
+        }else{
+            del_tree = pa->right_tree;
+            //delete del_tree;
+            pa->right_tree = NULL;
+        }
+    }
+    else if(ro->left_tree!=NULL || ro->right_tree!=NULL){
+        if(ro->left_tree!=NULL){
+            del_tree = pa->left_tree;
+            pa->left_tree = pa->left_tree->left_tree; 
+            delete del_tree;
+        }else if(ro->right_tree!=NULL){
+            del_tree = pa->right_tree;
+            pa->right_tree = pa->right_tree->right_tree; 
+            delete del_tree;
+        }
+    }
+    else if(ro->left_tree!=NULL && ro->right_tree!=NULL){
+        TreeNode<T>*st_node = ro->left_tree;
+        while(st_node!=NULL){
+            st_node=st_node->left_tree;
+        }
+        cout << *(st_node->value) << endl;
+
+
+    }
+    return 1;
+}
 template<typename T>
 int BSTree<T>::search(TreeNode<T>* root,T value){
     if(root==NULL){
@@ -27,6 +72,70 @@ int BSTree<T>::search(TreeNode<T>* root,T value){
         return search(root->right_tree,value);
     }
 }
+template<typename T>
+TreeNode<T>** BSTree<T>::search_return(TreeNode<T>* root,TreeNode<T>*parent,T value){
+    
+    if(root==NULL || parent==NULL){
+        return NULL;
+    }
+    if(*(root->value)==value){
+        TreeNode<T>** result = new TreeNode<T>*[2];
+        result[0] = root; 
+        result[1] = parent;
+        cout << root->value << endl;
+        cout << parent->value << endl;
+        return result;
+    }
+    else if(*(root->value)>value){
+        return search_return(root->left_tree,root,value);
+    }else{
+        return search_return(root->right_tree,root,value);
+    }
+}
+template<typename T>
+int BSTree<T>::insert_refer(TreeNode<T>* r,T&val){
+    TreeNode<T>* temp = this->root;
+    //validation
+    if(search(r,val)){
+        return -1;
+    }
+
+    while (temp!=NULL){
+        if (*(temp->value)<val){
+            if(temp->right_tree!=NULL){
+                temp = temp->right_tree;    
+            }else{
+                break;
+            }
+        }else{
+            if(temp->left_tree!=NULL){
+                temp = temp->left_tree; 
+            }else{
+                break;
+            }
+        }
+    }
+    TreeNode<T>* t = new TreeNode<T>;
+    t->value = &val;
+    t->left_tree = NULL;
+    t->right_tree = NULL;
+    if(*(temp->value)<val ){
+
+        temp->right_tree = t;
+    }else{
+        temp->left_tree = t;
+    }
+    return val;
+
+}
+template<typename T>
+int BSTree<T>::test_insert(TreeNode<T>* r,T val){
+    int* a = new int;
+    *a = val; 
+    insert_refer(r,*a);
+}
+
+
 
 template<typename T>
 int BSTree<T>::insert(TreeNode<T>* r,TreeNode<T>* v){
@@ -72,17 +181,9 @@ int BSTree<T>::insert(TreeNode<T>* r,TreeNode<T>* v){
     }
     if(*(temp->value)<*(v->value) ){
         
-        TreeNode<T>* t = new TreeNode<T>;
-        t->value = v->value;
-        t->left_tree = NULL;
-        t->right_tree = NULL;
-        temp->right_tree = t;
+        temp->right_tree = v;
     }else{
-        TreeNode<T>* t = new TreeNode<T>;
-        t->value = v->value;
-        t->left_tree = NULL;
-        t->right_tree = NULL;
-        temp->left_tree = t;
+        temp->left_tree = v;
     }
     return 1;
     
