af8644e62ac4e25c10e435d30dac1f9bcf101fc3
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-12-15 14:51:01 +0000

    feat : print heap, remove heap

--- DataStructure/PriorityQueue/PriorityQueue.cpp
+++ DataStructure/PriorityQueue/PriorityQueue.cpp
@@ -4,11 +4,24 @@
 using namespace std;
 
 int main(){
-    Heap heap = Heap();
+    Heap heap = Heap(MIN_HEAP);
     heap.insert(2);
     heap.insert(3);
+    heap.insert(6);
+    heap.insert(8);
+    heap.insert(1);
+    heap.insert(9);
+    heap.insert(-1);
 
-    cout << heap.heap[0] << heap.heap[1] << endl;
-//std::cout << heap.heap << endl;
+    // cout << heap.heap[1] << heap.heap[2] << heap.heap[3] << endl;
+    //cout << heap << endl;
+
+    cout << heap.remove();
+    cout << heap.remove();
+    cout << heap.remove();
+    cout << heap.remove();
+    cout << heap.remove();
+    cout << heap.remove();
+    cout << heap.remove();
 
 }
--- DataStructure/PriorityQueue/heap.hpp
+++ DataStructure/PriorityQueue/heap.hpp
@@ -1,37 +1,91 @@
 #include <iostream>
 
-#define MAX_HEAP 300
+#define HEAP_SIZE 300
+#define MAX_HEAP 1
+#define MIN_HEAP 0
 
 using namespace std;
 
 class Heap{
     public :
+        Heap(int );
         int insert(int );
+        int remove();
+        int remove_answer();
         int get_size();
         int get_root();
-        friend ostream& operator<<(ostream& outputStream, int value[MAX_HEAP]);
-        int heap[MAX_HEAP];
+        friend ostream& operator<<(ostream& outputStream, Heap&);
+        int heap[HEAP_SIZE];
     private :
         int heap_size = 0;
+        int heap_type = MAX_HEAP;
 };
+Heap::Heap(int heap_type=MAX_HEAP){
+    this->heap_type = heap_type;
+}
 
-ostream& operator<<(ostream& out,int value[MAX_HEAP]){
-    for(int i=0; value[i]!='\0'; i++){
-        out<<value[i] << " ";
+ostream& operator<<(ostream& out,Heap& heap){
+    int s = 1;
+    for(int i=1; i<=heap.heap_size; i++){
+        if (i==s*2){
+            cout << endl;
+            s*=2;
+
+        }
+        out<<heap.heap[i] << " ";
     }
+    return out;
 }
 
 int Heap::insert(int value){
-    if(heap_size==0){
-        heap[0] = value;
-        heap_size++;
-        return 1;
-    }
-    int low_pos = ++heap_size;
-    while(low_pos!=1 && heap[low_pos/2]<value){
+    int low_pos = ++(heap_size);
+    while(low_pos!=1 && (heap_type==MAX_HEAP?heap[low_pos/2]<value:heap[low_pos/2]>value)){
         heap[low_pos] = heap[low_pos/2];
         low_pos/=2;
     }
     heap[low_pos] = value;
     return 1;
+}
+int Heap::remove_answer(){
+    int parent = 1; int child =2;
+    int root,last;
+    root = heap[1];
+    last = heap[heap_size--];
+    while (child<=heap_size){
+        if(child<heap_size && heap[child*2]>heap[child*2+1]){
+            child++;
+        }
+        if(last>=heap[child]){
+            break;
+        }
+        heap[parent] = heap[child];
+        parent = child;
+        child *=2;
+    }
+    heap[parent] = last;
+    return root;
+}
+int Heap::remove(){
+    if(heap_size==0){
+        cout << "heap is empty !" << endl;
+        exit(1);
+    }
+    int pos = 1;
+    int temp;
+    int result = heap[pos];
+    heap[pos] = heap[heap_size--];
+    while(pos<heap_size){
+        if((heap_type==MAX_HEAP?heap[pos*2] > heap[pos*2+1]:heap[pos*2] < heap[pos*2+1])){
+            temp = heap[pos*2];
+            heap[pos*2] = heap[pos];
+            heap[pos] = temp;
+            pos*=2;
+        }else{
+            temp = heap[pos*2+1];
+            heap[pos*2+1] = heap[pos];
+            heap[pos] = temp;
+            pos = pos*2+1;
+        }
+    }
+    return result;
 }
