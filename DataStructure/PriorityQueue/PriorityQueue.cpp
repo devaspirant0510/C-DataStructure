832a7b134eb3357204b766c7d7cc81d1b4a8ef6d
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-12-16 13:45:49 +0000

    feat : heap sort

--- DataStructure/PriorityQueue/PriorityQueue.cpp
+++ DataStructure/PriorityQueue/PriorityQueue.cpp
@@ -3,25 +3,30 @@
 
 using namespace std;
 
+void heap_sort(int *,int);
+
 int main(){
-    Heap heap = Heap(MIN_HEAP);
-    heap.insert(2);
-    heap.insert(3);
-    heap.insert(6);
-    heap.insert(8);
-    heap.insert(1);
-    heap.insert(9);
-    heap.insert(-1);
-
-    // cout << heap.heap[1] << heap.heap[2] << heap.heap[3] << endl;
-    //cout << heap << endl;
-
-    cout << heap.remove();
-    cout << heap.remove();
-    cout << heap.remove();
-    cout << heap.remove();
-    cout << heap.remove();
-    cout << heap.remove();
-    cout << heap.remove();
+    int N = 10;
+    int arr[N];
+    for (int i=0; i<N; i++){
+        arr[i] = rand()%200;
+    }
+    heap_sort(arr,N);
+}
+void heap_sort(int * arr,int n){
+    Heap heap = Heap();
+    for(int i=0; i<n; i++){
+        heap.insert(arr[i]);
+    }
+    int res_arr[n];
+    for(int i=0; i<n; i++){
+        int val =heap.remove_answer();
+        res_arr[n-i] = val;
+       cout <<n-i<<" : "<< val << " " ;
+    }
+    for(int i=1; i<=n; i++){
+         cout << res_arr[i] << " " ;
+    }
+    cout << endl;
 
 }
