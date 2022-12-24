6d703e6724c307032fe5cfa514dd7e2b0d8de834
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-12-24 07:39:18 +0000

    feat : input and print

--- DataStructure/C_struct/Polynomial.hpp
+++ DataStructure/C_struct/Polynomial.hpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+#define MAX_DEGREE 100
+
 using namespace std;
 
+struct expression{
+    int degree;
+    float coef[MAX_DEGREE];
+};
 class Polynomial{
+    private:
+        expression exp;
+    public:
+        void input();
+        void print();
 
 };
+void Polynomial::input(){
+    int n;
+    int el;
+    cin >> n;
+    this->exp.degree = n;
+    for(int i=0; i<n; i++){
+        cin >> el;
+        this->exp.coef[i] = el;
+    }
+}
+void Polynomial::print(){
+    cout << "n: " << exp.degree;
+    for (int i=0; i<exp.degree; i++){
+        cout << exp.coef[i];
+    }
+}
--- DataStructure/C_struct/oper_polynomial.cpp
+++ DataStructure/C_struct/oper_polynomial.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "Polynomial.hpp"
+
 using namespace std;
 
 int main(){
+    Polynomial pol;
+    pol.input();
+    pol.print();
     return 0;
 }
